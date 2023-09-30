e130a6dc43641213759c4ff40d1f3038396d54da
VinayakPrakashh <vinayakprakash2121@gmail.com>
2023-09-30 11:30:52 +0530

    ggg

---
+++ main.py
@@ -0,0 +1,59 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+cap = cv2.VideoCapture(0)
+cap.set(3, 1288)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0)
+keys = [["Q","W","E","R","T","Y","U","I","Q","P"],
+        ["A","S","D","F","G","H","J","K","L",";"],
+        ["Z","X","C","V","B","N","M",",",".","/"]]
+
+def drawAll(img,ButtonList):
+        
+        for button in ButtonList:
+             
+            x,y=button.pos
+            w,h=button.size
+            cv2.rectangle(img,button.pos,(x + w,y + h),(255,0,255),cv2.FILLED)
+            cv2.putText(img,button.text,(x+21,y+ 65),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),4)
+        return img
+
+class Button():
+    def __init__(self, pos, text, size=[85,85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+        
+        
+
+ButtonList = []
+for i in range(len(keys)):
+        for j, key in enumerate(keys[i]):
+            ButtonList.append(Button([100 * j + 58,100 * i + 58], key))
+
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmlist, bboxInfo = detector.findPosition(img)
+    img = drawAll(img, ButtonList)
+
+    if lmlist:
+         for button in ButtonList:
+              x,y = button.pos
+              w,h = button.size
+
+              if x<lmlist[8][0]<x+w and y<lmlist[8][1]<y+h:
+                   cv2.rectangle(img,button.pos,(x + w,y + h),(175,0,175),cv2.FILLED)
+                   cv2.putText(img,button.text,(x+21,y+ 65),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),4)
+                   l,_,_ = detector.findDistance(8,12,img,draw=False)
+                   print(l)
+
+                   if l<38:
+                         
+                         cv2.rectangle(img,button.pos,(x + w,y + h),(0,255,0),cv2.FILLED)
+                         cv2.putText(img,button.text,(x+21,y+ 65),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),4)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
